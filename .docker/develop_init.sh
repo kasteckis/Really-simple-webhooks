7c29be16c7a9028f23551c3683733227425d98af
Valentinas Kasteckis <plankton546@gmail.com>
2020-08-31 20:29:06 +0300

    Symfony init, connected with npm packages

---
+++ .docker/develop_init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+sleep 10
+cd /var/www/html
+composer install
+npm install
+npm run scss
+#php bin/console d:s:u --force
+#php bin/console doctrine:fixtures:load --append
+echo "Project has been started"
+echo "Access project website - http://192.168.4.2/"
+echo "MYSQL Server - 192.168.4.3:3306"
+echo "Access phpmyadmin via - http://192.168.4.4/"
+echo "Access email server via - http://192.168.4.5:8025/"
+echo "Access server SSH by writing - docker exec -it trigger_web bash"
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+import '../css/app.css';
+
 (function($) {
 
 	"use strict";
